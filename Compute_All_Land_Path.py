ce8967cef77ea9bddb3ce59b180f74b5d8a4129d
Teayyyy <zgsighoty@outlook.com>
2023-06-21 23:31:59 +0800

    添加了功能：通过计算各个耕作方向，计算地头面积和地块总面积之比，选择最小地头面积的耕作方向get_all_land_with_min_headland_2()

--- Compute_All_Land_Path.py
+++ Compute_All_Land_Path.py
@@ -3,10 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import geopandas as gpd
 import datetime
+import warnings
 
 """
 用来一次性规划所有地块的路径规划，并显示到图上
 """
+
+warnings.filterwarnings('ignore')
+
+mean_slope = [3.37, 7.38, 6.07, 3.94, 6.53, 7.46, 7.62]
 
 
 def get_all_land_path():
@@ -116,6 +121,51 @@ def get_all_land_path_with_min_headland(swath_width, along_long_edge=True, head_
     plt.savefig('Saved_Result/CPP_{}.pdf'.format(now))
 
 
+def get_all_land_with_min_headland_2(swath_width, head_land_width=6):
+    all_land = CPP_Planner_Kit.load_fields('Scratch/test_Load_Shp/shp_file/村1地_全区.shp')
+    num_land = len(all_land)
+    print("田块的个数: ", num_land)
+
+    all_land_path = []
+    all_headland = []
+    # 需要将每一个地块分解、规划路径
+    for index in range(num_land):
+        print("land: ", index + 1)
+        slope = mean_slope[index]
+        print("field Slope: ", slope)
+        this_swath_width = CPP_Planner_Kit.get_corrected_swath_width(swath_width, slope)
+        single_land = CPP_Planner_Kit.get_single_shp(all_land, index)
+        split_polygon = CPP_Planner_Kit.split_polygon_by_largest_area(single_land.geometry.iloc[0], tolerance=0.05)
+        # print(type(split_polygon))
+        # 对每一个分割开的多边形地块子块进行路径规划
+        for polygon in split_polygon:
+            if polygon.area > 20:
+                polygon_land = gpd.GeoDataFrame(geometry=[polygon], crs=all_land.crs)
+                # t_path, t_headland = CPP_Algorithm_Optimizers.gen_path_with_minimum_headland_area(
+                #     polygon_land, step_size=swath_width, head_land_width=head_land_width)
+                t_path, t_headland = CPP_Algorithm_Optimizers.gen_path_with_minimum_headland_area_by_direction(
+                    land=polygon_land, step_size=this_swath_width, head_land_width=head_land_width
+                )
+                all_land_path.append(t_path)
+                all_headland.append(t_headland)
+
+    # 显示所有的地块和地头
+    fig, ax = plt.subplots()
+    # all_land.plot(ax=ax, color='b')
+    exte = all_land.exterior
+    exte.plot(ax=ax, color='b', linewidth=0.3)
+    for path in all_land_path:
+        path.plot(ax=ax, color='y', linewidth=0.2)
+    for headland in all_headland:
+        headland.plot(ax=ax, color='gray')
+
+    # plt.show()
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    plt.savefig('Saved_Result/CPP_minareadi_{}.pdf'.format(now))
+
+
+# correct_swath_width = CPP_Planner_Kit.get_corrected_swath_width(swath_width=1.45, slope=6)
 # get_all_land_path()
 # get_all_land_path_with_headland(swath_width=1.45, along_long_edge=True, headland_mode='left', head_land_width=6)
-get_all_land_path_with_min_headland(swath_width=1.45, along_long_edge=True, head_land_width=6)
+# get_all_land_path_with_min_headland(swath_width=1.45, along_long_edge=True, head_land_width=6)
+get_all_land_with_min_headland_2(swath_width=1.45, head_land_width=6)
