5dad9788db26b3c6594289873a2d9c1c6037de23
Teayyyy <zgsighoty@outlook.com>
2023-06-29 03:41:15 +0800

    添加了功能：使用 flat turn & fishtail 转向进行路径规划的转向规划，细节见 CPP_Planner

--- Compute_All_Land_Path.py
+++ Compute_All_Land_Path.py
@@ -210,8 +210,9 @@ def get_all_land_with_min_headland_edge_with_turnings(swath_width):
     turning_radius = 4.5
     vehicle_length = 6.3
     vehicle_width = 1.9
-    headland_width, vehicle_theta = CPP_Planner_Kit.calc_headland_width(turning_radius, swath_width, vehicle_length, vehicle_width,
-                                                         buffer=0.3, show_info=True)
+    headland_width, vehicle_theta = CPP_Planner_Kit.calc_headland_width(turning_radius, swath_width, vehicle_length,
+                                                                        vehicle_width,
+                                                                        buffer=0.3, show_info=True)
 
     all_land = CPP_Planner_Kit.load_fields('Scratch/test_Load_Shp/shp_file/村1地_全区.shp')
     num_land = len(all_land)
@@ -275,10 +276,81 @@ def get_all_land_with_min_headland_edge_with_turnings(swath_width):
     pass
 
 
+def get_all_land_with_min_headland_edge_with_flat_fishtail_turnings(swath_width):
+    turning_radius = 4.5
+    vehicle_length = 6.3
+    vehicle_width = 1.9
+    headland_width, vehicle_theta = CPP_Planner_Kit.calc_headland_width(turning_radius, swath_width, vehicle_length,
+                                                                        vehicle_width,
+                                                                        buffer=0.3, show_info=True)
+
+    all_land = CPP_Planner_Kit.load_fields('Scratch/test_Load_Shp/shp_file/村1地_全区.shp')
+    num_land = len(all_land)
+    print("田块的个数: ", num_land)
+
+    all_land_path = []
+    all_headland = []
+    all_flat_turns = []
+    all_fishtail_turns = []
+    for index in range(num_land):
+        print("Land no: ", index + 1)
+        slope = mean_slope[index]
+        temp_corrected_swath_length = CPP_Planner_Kit.get_corrected_swath_width(swath_width, slope)
+        single_land = CPP_Planner_Kit.get_single_shp(all_land, index)
+        # 分割田块为简单的多边形
+        split_polygon = CPP_Planner_Kit.split_polygon_by_largest_area(single_land.geometry.iloc[0], tolerance=0.05)
+        # 对分割的每一个多边形进行路径规划，这一次使用的是从多边形的每一边开始顺着边开始计算路径，找到各边中地头占据面积最小的区域
+        for polygon in split_polygon:
+            if polygon.area > 20:
+                # 计算当前多边形的每一条边，顺着进行路径规划，找到地头占据面积最小的那一条边
+                polygon_regen = gpd.GeoDataFrame(geometry=[polygon], crs=all_land.crs)
+                polygon_centroid = polygon_regen.centroid[0]
+                temp_path, temp_headland, theta = CPP_Algorithm_Optimizers.gen_path_with_minimum_headland_area_by_edge(
+                    land=polygon_regen, step_size=temp_corrected_swath_length, head_land_width=headland_width,
+                    headland_mode='both', compare_mode='headland', return_theta=True
+                )
+                print(theta)
+                # 计算地头的转向等路径
+                flat_turn_paths, fishtail_paths = CPP_Planner_TurningRail_Maker.gen_path_flat_turn_tail_turn(
+                    temp_path, turning_radius, vehicle_length, vehicle_width, swath_width, polygon_centroid, theta
+                )
+                all_flat_turns += flat_turn_paths
+                all_fishtail_turns += fishtail_paths
+
+                all_land_path.append(temp_path)
+                all_headland.append(temp_headland)
+
+    # 显示所有的地头和路径在地块内，或保存
+    _, ax = plt.subplots()
+    exte = all_land.exterior
+    exte.plot(ax=ax, color='b', linewidth=0.3)
+    for path in all_land_path:
+        path.plot(ax=ax, color='y', linewidth=0.2)
+    for headland in all_headland:
+        headland.plot(ax=ax, color='gray')
+    for fishtail_turn in all_fishtail_turns:
+        fishtail_turn.plot(ax=ax, color='g', linewidth=0.1)
+    for flat_turn in all_flat_turns:
+        flat_turn.plot(ax=ax, color='black', linewidth=0.1)
+    # for headland_move in all_headland_move:
+    #     headland_move.plot(ax=ax, color='orange', linewidth=0.1)
+    # for turning_line in all_turning_curves:
+    #     turning_line.plot(ax=ax, color='green', linewidth=0.1)
+    # for backward_line in all_backward_moves:
+    #     backward_line.plot(ax=ax, color='green', linewidth=0.1)
+
+    # plt.show()
+    now = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    plt.savefig('Saved_Result/CPP_min_edge_turning_{}.pdf'.format(now))
+    print("Saving Name: ", 'CPP_min_edge_fishtail_flatturn_{}.pdf'.format(now))
+    pass
+
+
 # correct_swath_width = CPP_Planner_Kit.get_corrected_swath_width(swath_width=1.45, slope=6)
 # get_all_land_path()
 # get_all_land_path_with_headland(swath_width=1.45, along_long_edge=True, headland_mode='left', head_land_width=6)
 # get_all_land_path_with_min_headland(swath_width=1.45, along_long_edge=True, head_land_width=6)
 # get_all_land_with_min_headland_2(swath_width=1.45, head_land_width=6)
 # get_all_land_with_min_headland_edge(swath_width=1.45, head_land_width=6)
-get_all_land_with_min_headland_edge_with_turnings(1.45)
+# get_all_land_with_min_headland_edge_with_turnings(1.45)
+get_all_land_with_min_headland_edge_with_flat_fishtail_turnings(1.45)
